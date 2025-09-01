a95462a4e82ab9ecc147151fb6f0167eb085d507
valentinFontaine <valentin.fontaine@protonmail.com>
2025-09-01 20:23:23 +0200

    added developper mode full

---
+++ config.py
@@ -0,0 +1,7 @@
+"""
+Configuration settings for the TaskWarrior Web UI
+"""
+
+# Developer mode settings
+DEVELOPER_MODE = False  # Set to False to disable developer mode
+DEBUG_FILE = 'command.debug'
